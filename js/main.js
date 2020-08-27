f419754e8622ee6a94f53d8ef5cd226e816334ca
shalabycr7 <cr7.shalaby@gmail.com>
2020-08-27 16:46:03 +0200

    f

--- js/main.js
+++ js/main.js
@@ -1,15 +1,12 @@
-var footer=document.getElementById('main-footer');
-footer.addEventListener('mousemove', function(){
-   alert('hh');
-})
 
- window.addEventListener('scroll',(event) => { 
- console.log(Math.round(window.scrollY));
-   if(Math.round(window.scrollY)==1958){
-      
-      let topButton=document.getElementById('top-bu');
+ window.onscroll = function(ev) {
+    
+    if ((window.innerHeight + window.scrollY) >= document.body.scrollHeight) {
+   // you're at the bottom of the page 
+   
+   let topButton=document.getElementById('top-bu');
       topButton.style.display='block';
       topButton.addEventListener('click',function(){topButton.style.display='none'})
-   }
- });
+   
+} };
 
